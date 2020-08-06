d870b04e5669719844e32316b50385b608dd4dc8
Ali Haider Rizvi <45711004+alihaiderrizvi@users.noreply.github.com>
2020-08-06 15:05:58 +0500

    ✨Add slider

--- game.py
+++ game.py
@@ -14,8 +14,48 @@ pygame.display.set_icon(icon)
 #setting game state
 is_running = True
 
+# slider
+slider_img = pygame.image.load('assets/slider.png')
+sliderX = 250
+sliderY = 530
+sliderX_change = 0
+
+def slider(x,y):
+    screen.blit(slider_img, (x, y))
+
 #game loop
 while is_running:
+
+    #Set background color
+    screen.fill((255, 255, 255)) # (Red, Green, Blue)
+
     for event in pygame.event.get():
+        #check game quit
         if event.type == pygame.QUIT:
             is_running = False
+        
+        
+        if event.type == pygame.KEYDOWN:
+            #check left
+            if event.key == pygame.K_LEFT:
+                sliderX_change = -0.2
+            
+            #check left
+            if event.key == pygame.K_RIGHT:
+                sliderX_change = 0.2
+        
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                sliderX_change = 0
+        
+    sliderX += sliderX_change
+    
+    #resetting position if slider goes out of screen
+    if sliderX < 0:
+        sliderX = 0
+    elif sliderX >= 545:
+        sliderX = 545
+    
+    slider(sliderX, sliderY) # display slider
+    
+    pygame.display.update() # updating screen
